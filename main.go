060f879a55d112e5dbd02740461010138d1497d5
srhtylmz19 <srhtylmz19@gmail.com>
2020-03-06 01:04:33 +0300

    updates

--- main.go
+++ main.go
@@ -5,14 +5,18 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
+// Article is ...
 type Article struct {
 	Title   string `json:"Title"`
 	Desc    string `json:"desc"`
 	Content string `json:"content"`
 }
 
+// Articles is ...
 type Articles []Article
 
 func allArticles(w http.ResponseWriter, r *http.Request) {
@@ -24,14 +28,22 @@ func allArticles(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(articles)
 }
 
+func testPostArticles(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "POST METHOD CALLED")
+}
+
 func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Home page fucn called")
+	fmt.Fprintf(w, "home page func called")
 }
 
 func handleRequest() {
-	http.HandleFunc("/", homePage)
-	http.HandleFunc("/articles", allArticles)
-	log.Fatal(http.ListenAndServe(":8081", nil))
+
+	myRouter := mux.NewRouter().StrictSlash(true)
+
+	myRouter.HandleFunc("/", homePage)
+	myRouter.HandleFunc("/articles", allArticles).Methods("GET")
+	myRouter.HandleFunc("/articles", testPostArticles).Methods("POST")
+	log.Fatal(http.ListenAndServe(":8081", myRouter))
 }
 
 func main() {
